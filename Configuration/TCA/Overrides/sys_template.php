bfafafc1a1e4eaffd9344885192b96478fcb5fe3
Juergen Kussmann <juergen.kussmann@aoe.com>
2017-11-02 08:20:20 +0100

    [CLEANUP] don't configure TCA in ext_tables.php

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,31 +1,35 @@
 <?php
+if (!defined('TYPO3_MODE')) {
+    die('Access denied.');
+}
 
+// Register static TypoScript templates
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'imgix',
     'Configuration/Static/',
     'imgix: Common Constants'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'imgix',
     'Configuration/Static/Angular/Plugin/',
     'imgix: Load Angular Extbase-Plugin for further usage'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'imgix',
     'Configuration/Static/Angular/Include/',
     'imgix: Include Angular-Module files into page'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'imgix',
     'Configuration/Static/JQuery/Plugin/',
     'imgix: Load Jquery Extbase-Plugin for further usage'
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-    $_EXTKEY,
+    'imgix',
     'Configuration/Static/JQuery/Include/',
     'imgix: Include Jquery-Plugin files into page'
 );
