6fa14c82d70d5f9d1ab834bb4c92f9a557601906
Christian Krzeminski <christian.krzeminski@aoe.com>
2019-06-05 15:09:32 +0200

    [TASK] Update dependencies for TYPO3V8

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '3.0.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '7.6.0-8.7.99',
+            'typo3' => '8.7.0-8.7.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
